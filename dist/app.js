43e3b52605b48140030ba219e3f44a085595f75f
tubanursmsk <tubanursmsk@gmail.com>
2025-09-25 13:49:18 +0300

    logim-register

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,10 @@
-import express from 'express'; //expres: web server oluşturmak için ya da API oluşturmak için kullanılır
-const app = express(); //
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express")); //expres: web server oluşturmak için ya da API oluşturmak için kullanılır
+const app = (0, express_1.default)(); //
 const PORT = process.env.PORT || 3000; //process.env.PORT: ortam değişkenlerinden port numarasını alır, yoksa 3000 kullanır
 app.listen(PORT, () => {
     console.log(`Server is running on port: http://localhost:${PORT}`);
